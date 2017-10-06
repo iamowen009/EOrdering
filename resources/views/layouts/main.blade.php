929908d1dc50636345ab920719ec4c72662e7eb0
Saimok <nong.wasantasiri@gmail.com>
2017-10-06 07:28:34 +0700

    fixed main

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -193,7 +193,7 @@
                                         <ul class="dropdown-menu">
                                             <li ng-hide="hidemenu"><a href="#" ng-click="toPage('<?php echo url('profile') ?>')">ข้อมูลส่วนตัว</a></li>
                                             <li ng-hide="hidemenu"><a href="#" ng-click="toPage('<?php echo url('contact')  ?>')">ติดต่อเรา</a></li>
-                                            <li ng-hide="hidemenu"><a href="#" ng-click="toPage('<?php echo url('profile-update)')  ?>'">แจ้งเรื่องเปลี่ยนแปลงข้อมูล</a></li>
+                                            <li ng-hide="hidemenu"><a href="#" ng-click="toPage('<?php echo url('profile-update')  ?>')">แจ้งเรื่องเปลี่ยนแปลงข้อมูล</a></li>
                                             <li ng-hide="hidemenu"><a href="#" ng-click="toPage('<?php echo url('store')  ?>')">เปลี่ยนร้านค้า</a></li>
                                             <li ng-hide="hidemenu"><a href="#" ng-click="toPage('<?php echo url('password-update')  ?>')">เปลี่ยนรหัสผ่าน</a></li>
                                             <li><a ng-click="$event.preventDefault(); logout()">ออกจากระบบ</a></li>
