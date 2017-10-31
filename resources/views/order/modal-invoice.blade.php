f6d0000df87200eec6ffd2be1e01c81551c90f46
Saimok <nong.wasantasiri@gmail.com>
2017-10-31 08:19:22 +0700

    INVOICE-UI-01

--- resources/views/order/modal-invoice.blade.php
+++ resources/views/order/modal-invoice.blade.php
@@ -4,118 +4,120 @@
     <!-- Modal content-->
     <div class="modal-content">
       <div class="modal-header info">
-      	<div class="col-sm-1">
-        <button type="button" class="close" data-dismiss="modal">&times;</button>
-    	</div>
-    	<div class="col-sm-11 text-center">
-        	<h4 class="modal-title">ใบสั่งซื้อ</h4>
-        </div>
+      	<div class="col-sm-12">
+            <button type="button" class="close" data-dismiss="modal">&times;</button>
+    	  </div>
       </div>
       <div class="modal-body">
-        <!--<div class="bg-gray">
-
-        </div>-->
-        <div class="x_panel" style="padding:0;">
-            <div class="x_title" style="margin:0;">
-                <h4 class="ng-binding inv-customer"> @{{ inv.customerCode + ' ' + inv.customerName}}</h4>
-                <div class="clearfix"></div>
-            </div>
-            <div class="x_content" style="padding:10px; margin:0;">
-                  <form class="form-horizontal ng-pristine ng-valid">
-                      <div class="form-group col-md-6">
-                          <label for="email" class="col-md-5 text-right">เลขที่ใบสั่งซื้อ : </label>
-                          <label class="col-md-7 text-left ng-binding inv-documentNumber">@{{ inv.documentNumber }}</label>
-                      </div>
-                      <div class="form-group col-md-6">
-                            <label for="pwd" class="col-md-5 text-right">ที่อยู่ :</label>
-                            <label class="col-md-7 text-left ng-binding inv-address">@{{ inv.address }}@{{ inv.districtName }}@{{ inv.cityName }}@{{ inv.postCode }}</label>
-                      </div>
-                      <div class="form-group col-md-6">
-                            <label for="email" class="col-md-5 text-right">วันที่สั่งซื้อ :</label>
-                            <label class="col-md-7 text-left ng-binding inv-documentDate">@{{ inv.requestDate | date:'dd/mm/yyy' }}</label>
-                      </div>
-                      <div class="form-group col-md-6">
-                            <label for="pwd" class="col-md-5 text-right">อีเมลล์ :</label>
-                            <label class="col-md-7 text-left ng-binding inv-email"></label>
-                      </div>
-                      <div class="form-group col-md-6">
-                            <label for="email" class="col-md-5 text-right">การชำระเงิน :</label>
-                            <label class="col-md-7 text-left inv-paymentTerm">@{{ inv.paymentTerm === 'CASH' ? 'เงินสด' :( inv.paymentTerm === 'CREDIT' ? 'เครดิต' : '' ) }}</label>
-                      </div>
-                      <div class="form-group col-md-6">
-                            <label for="pwd" class="col-md-5 text-right">เบอร์โทรศัพท์ :</label>
-                            <label class="col-md-7 text-left ng-binding inv-customerTelNo">@{{ inv.customerTelNo }}</label>
-                      </div>
-                      <div class="form-group col-md-6">
-                            <label for="email" class="col-md-5 text-right">ขนส่งโดย :</label>
-                            <label class="col-md-7 text-left">@{{ shipto(inv.shipCondition) }}</label>
-                      </div>
-                      <div class="form-group col-md-6">
-                            <label for="pwd" class="col-md-5 text-right">บริษัทขนส่ง :</label>
-                            <label class="col-md-7 text-left ng-binding">@{{ inv.transportZoneDesc }}</label>
-                      </div>
-                      <div class="form-group col-md-6">
-                            <label for="email" class="col-md-5 text-right">สถานที่ส่ง :</label>
-                            <label class="col-md-7 text-left ng-binding">@{{ inv.shipHouseNo }} @{{ inv.shipAddress }} @{{ inv.shipDistrictName }} @{{ inv.shipCityName }} @{{ inv.shipPostCode }}</label>
-                      </div>
-                      <div class="form-group col-md-6">
-                            <label for="pwd" class="col-md-5 text-right">วันที่ต้องการ :</label>
-                            <label class="col-md-7 text-left ng-binding">@{{ inv.requestDate | date:'dd/mm/yyyy' }}</label>
-                      </div>
-                      <div class="form-group col-md-6">
-                            <label for="email" class="col-md-5 text-right">ที่อยู่สถานที่ส่ง :</label>
-                            <label class="col-md-7 text-left ng-binding">@{{ inv.shipName }}</label>
-                      </div>
-                      <div class="form-group col-md-6">
-                            <label for="pwd" class="col-md-5 text-right">เลขที่ PO :</label>
-                            <label class="col-md-7 text-left ng-binding">@{{ inv.customerPO}}</label>
-                      </div>
-                  </form>
-              </div>
+        <div class="row invoice-header">
+          <div class="col-md-8">
+            <img src="<?php echo asset('images/toa-logo-blank.png'); ?>" class="pull-left" style="height:80px;"/>
+            <h4>บริษัท ทีโอเอ เพ้นท์ (ประเทศไทย) จำกัด (มหาชน)</h4>
+            <p>สำนักงาน และศูนย์อุตสาหกรรม ทีโอเอ บางนา-ตราด 31/2 หมู่ 3 </p>
+            <p>ถนนบางนา-ตราด ตำบลบางเสาธง อ.บางเสาธง จ.สมุทรปราการ 10570</p>
+          </div>
+          <div class="col-md-4 text-right">
+            <p>วันที่ : <strong>@{{ inv.requestDate | date:'dd/MM/yy'  }}</strong></p>
+            <p>เลขที่ใบสั่งซื้อ : <strong>@{{ inv.documentNumber }}</strong></p>
+          </div>
         </div>
+        <h4 class="text-center modal-title">ใบสั่งซื้อ</h4>
+        <div class="" style="padding:0;">
+          <div class="row form-horizontal">
+            <div class="col-md-6" >
+              <div class="x_panel">
+              <h4 class="ng-binding inv-customer"> @{{ inv.customerName }}</h4>
+              <div class="form-group">
+                    <span for="pwd" class="col-md-4">ที่อยู่ :</span>
+                    <span class="col-md-8 text-left ng-binding inv-address">@{{ inv.address }}@{{ inv.districtName }}@{{ inv.cityName }}@{{ inv.postCode }}</span>
+              </div>
+              <div class="form-group">
+                    <span for="pwd" class="col-md-4">เบอร์โทรศัพท์ :</span>
+                    <span class="col-md-8 text-left ng-binding inv-customerTelNo">@{{ inv.customerTelNo }}</span>
+              </div>
+              <div class="form-group">
+                    <span for="pwd" class="col-md-4">อีเมลล์ :</span>
+                    <span class="col-md-8 text-left ng-binding inv-email"></span>
+              </div>
+              <div class="form-group">
+                    <span for="pwd" class="col-md-4">บริษัทขนส่ง :</span>
+                    <span class="col-md-8 text-left ng-binding">@{{ inv.transportZoneDesc }}</span>
+              </div>
 
-        <h6>รายละเอียดสินค้าที่สั่งซื้อ</h6>
-        <div class="invoice-block">
-            <table class="table table-hover text-right">
+            </div>
+            </div>
+            <div class="col-md-6 ">
+              <div class="x_panel">
+              <div class="form-group">
+                    <span for="email" class="col-md-4">การจัดส่ง :</span>
+                    <span class="col-md-8 text-left">@{{ shipto(inv.shipCondition) }}</span>
+              </div>
+
+              <div class="form-group">
+                    <span for="email" class="col-md-4">การชำระเงิน :</span>
+                    <span class="col-md-8 text-left inv-paymentTerm">@{{ inv.paymentTerm === 'CASH' ? 'เงินสด' :( inv.paymentTerm === 'CREDIT' ? 'เครดิต' : '' ) }}</span>
+              </div>
+
+
+
+              <div class="form-group">
+                    <span for="email" class="col-md-4">สถานที่ส่ง :</span>
+                    <span class="col-md-8 text-left ng-binding">@{{ inv.shipHouseNo }} @{{ inv.shipAddress }} @{{ inv.shipDistrictName }} @{{ inv.shipCityName }} @{{ inv.shipPostCode }}</span>
+              </div>
+              <div class="form-group">
+                    <span for="email" class="col-md-4">ที่อยู่สถานที่ส่ง :</span>
+                    <span class="col-md-8 text-left ng-binding">@{{ inv.shipName }}</span>
+              </div>
+              <div class="form-group">
+                    <span for="pwd" class="col-md-4">วันที่ต้องการ :</span>
+                    <span class="col-md-8 text-left ng-binding">@{{ inv.requestDate+543 | date:'dd/MM/yyyy' }}</span>
+              </div>
+              <div class="form-group">
+                    <span for="pwd" class="col-md-4">เลขที่ PO :</span>
+                    <span class="col-md-8 text-left ng-binding">@{{ inv.customerPO}}</span>
+              </div>
+            </div>
+            </div>
+          </div>
+        </div>
+        <div class="invoice-block row">
+            <table class="table table-hover table-bordered">
                 <thead class="thead-default">
                     <tr>
                         <th class="text-center">#</th>
-                        <th class="text-center">รหัสสินค้า</th>
-                        <th class="text-center">สินค้า</th>
+                        <th class="text-center">ผลิตภัณฑ์</th>
                         <th class="text-right">จำนวน</th>
-                        <th class="text-center">หน่วย</th>
+                        <!-- <th class="text-center">หน่วย</th> -->
                         <th class="text-right">ราคาหน่วย</th>
                         <th class="text-right">ราคารวม</th>
                     </tr>
                 </thead>
-                <tfoot>
 
-                </tfoot>
                 <tbody>
                 <tr ng-repeat="item in detail">
                     <td class="text-center">@{{$index+1}}</td>
-                    <td class="text-center"><img class="img-product" src="@{{partImgProduct}}/@{{item.btfCode}}.jpg" err-SRC="@{{partImgProduct}}/Noimage.jpg"> @{{item.productCode}}</td>
-                    <td class="text-center">@{{ item.productNameTh }}</td>
-                    <td class="text-right">@{{ item.qty | number }}
-                    </td>
-                    <td class="text-center">@{{item.unitNameTh}}</td>
-                    <td>@{{ item.amount | number:2}}</td>
+                    <!-- <td class="text-center"><img class="img-product" src="@{{partImgProduct +'/'+ item.btfCode}}.jpg" err-SRC="@{{partImgProduct}}/Noimage.jpg"> </td> -->
+                    <td class="text-left">@{{item.productCode}} @{{ item.productNameTh }}</td>
+                    <td class="text-right">@{{ item.qty | number }} @{{item.unitNameTh}}</td>
+                    <!-- <td class="text-center">@{{item.unitNameTh}}</td> -->
+                    <td class="text-right">@{{ item.amount | number:2}}</td>
 
-                    <td>@{{ +item.amount*+item.qty | number:2 }}</td>
+                    <td class="text-right">@{{ +item.amount*+item.qty | number:2 }}</td>
 
                 </tr>
                 </tbody>
+                <tfoot>
+                  <tr>
+                    <td class="text-center"></td>
+                    <td class="text-center page-invoice-amount"><strong>ยอดรวมมูลค่าสินค้า(ไม่รวม VAT):</strong></td>
+                    <td class="text-center"><strong><span class="">@{{ totalQty | number:0 }} รายการ</span> </strong></td>
+                    <td class="text-center"><strong><span class=""></span> </strong></td>
+                    <td class="text-center"><strong><span class="">@{{ totalAmount | number:2 }}</span> </strong></td>
+                </tr>
+              </tfoot>
             </table>
 
         </div>
-        <div class="text-right clearfix">
-            <div class="pull-right">
-                <p class="page-invoice-amount">
-                    <strong>ยอดรวมมูลค่าสินค้า(ไม่รวม VAT): <span class="total_price">@{{ totalAmount | number:2 }}</span> บาท</strong>
-                </p>
-                <br>
-            </div>
-          </div>
           <p style="color:red;">ราคาต่อหน่วยหลังหักราคามาตรฐานเท่านั้น</p>
       </div>
 
