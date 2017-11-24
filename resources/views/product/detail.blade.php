a112ed50e9ceb0be5972def882225ab7bd93a524
สุธรรม <sutham@buinnovation.com>
2017-11-24 10:51:53 +0700

    แก้ duplicate สี

--- resources/views/product/detail.blade.php
+++ resources/views/product/detail.blade.php
@@ -92,7 +92,8 @@
 											</button>
 											<ul class="dropdown-menu">
 												<!-- <li ng-repeat="p in colors"> -->
-												<li ng-repeat="p in colors | orderBy:'colorCode'">
+												<!-- <li ng-repeat="p in colors | orderBy:'colorCode'"> -->
+												<li ng-repeat="p in listColors | orderBy:'colorCode'">
 													<a ng-click="setProduct(p.colorCode)" ng-model="cartColor" value="{{ p.colorCode }}">
 														<span class="frame-color" ng-show="p.cartrgbColor!=''" style="background-color: rgb({{p.cartrgbColor}});"></span>
 														<span ng-show="p.cartrgbColor==''" class="frame-color" style="background-color: #fff;"></span>
