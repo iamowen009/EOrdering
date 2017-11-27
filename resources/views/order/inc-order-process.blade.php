74f2e7d3cab7f4dc686bab072446ab30e863d6f5
สุธรรม <sutham@buinnovation.com>
2017-11-27 11:52:03 +0700

    ทำ cancel ให้คลิกได้

--- resources/views/order/inc-order-process.blade.php
+++ resources/views/order/inc-order-process.blade.php
@@ -69,7 +69,7 @@
                       <!--   <span style="color:green">Complete</span> -->
                     </a>
 
-                    <a ng-show="(list.percentComplete===0 ) && (list.rejectHStatus==='C')"> 
+                    <a href="javascript:void(0)" ng-show="(list.percentComplete===0 ) && (list.rejectHStatus==='C')" ng-click="OrderDetailModal(list.orderId,list.salesOrderNumber)"> 
                       <div class="progress">
                       <div class="progress-bar" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style="width:100%; background-color:red; ">Cancelled</div>
                       </div>
